a467c2164127ffc57ef09c411db16221339fcbaf
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-16 19:35:46 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -18,7 +18,7 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  
 end
 
 def multiplication(num1, num2)
@@ -27,7 +27,7 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  remainder = num1  num2
+  remainder = num2 % num1
   return remainder
 end
 
